24b78dc54859358c57f7b9430c4277fdac73a0f1
Andrew Kopf <magathia@gmail.com>
2024-07-28 06:56:50 -0700

    DAO annotation added.

--- app/src/main/java/com/example/weathermon/database/dao/LocationDAO.java
+++ app/src/main/java/com/example/weathermon/database/dao/LocationDAO.java
@@ -1,5 +1,6 @@
 package com.example.weathermon.database.dao;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
@@ -10,6 +11,7 @@ import com.example.weathermon.database.entities.Location;
 
 import java.util.List;
 
+@Dao
 public interface LocationDAO {
 
     @Insert
